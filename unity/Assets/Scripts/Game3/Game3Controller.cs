540ce563c9bf105dac01f9715e85ac50038bcaf8
seestrifort <seestrifort@gmail.com>
2015-09-06 21:46:08 +0900

    Game3追加

---
+++ unity/Assets/Scripts/Game3/Game3Controller.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game3Controller : GameController {
+
+	[SerializeField] 
+	public Game3UI gameUI;
+
+	public Game3Icon[] puzzles;
+
+	int level = 1;
+	int correct = 0;
+	int wrong = 0;
+	int combo = 0;
+	int maxcombo = 0;
+	
+	protected override void Awake (){
+		base.Awake();
+	}
+	
+	public override void DataLoaded (){
+		gameTimer.SetTimer(Master.game2Data.timerLimit);
+
+		GameSet();
+	}
+
+	void GameSet(){
+
+		GameStart();
+	}
+
+	public override void GameStart () {
+
+		base.GameStart ();
+	}
+	
+	void Correct(){
+		correct ++;
+		combo ++;
+		maxcombo = Mathf.Max(maxcombo, combo);
+		StartCoroutine(CorrectAnimation());
+	}
+	
+	IEnumerator CorrectAnimation(){
+		gameTimer.timerFlag ++;
+		
+		if(level != correct/Master.game2Data.levelupSpan + 1){
+			
+			level = correct/Master.game2Data.levelupSpan + 1;
+
+			gameUI.messageLabel.text = "level up ! \n level : " + level;
+			
+			yield return new WaitForSeconds(0.5f);
+			
+			gameUI.messageLabel.text = "";
+		}
+	
+		gameTimer.timerFlag --;
+	}
+	
+	void Wrong(){
+		wrong ++;
+		combo = 0;
+		
+		StartCoroutine(WrongAnimation());
+	}
+	
+	IEnumerator WrongAnimation(){
+		gameTimer.timerFlag ++;
+		
+		gameUI.messageLabel.text = "INCORRECT !";
+		
+		yield return new WaitForSeconds(0.5f);
+		
+		gameUI.messageLabel.text = "";
+
+		gameTimer.timerFlag --;
+	}
+
+	public override void GameEnd (){
+		base.GameEnd ();
+		
+		gameUI.messageLabel.text = "GameOver";
+		
+		gameUI.retryButton.SetActive(true);
+	}
+	
+	public override void TimeOver (){
+		GameEnd();
+	}
+
+	public void Retry(){
+		Application.LoadLevel(Constants.Scene.MyPage);
+	}
+}
---
+++ unity/Assets/Scripts/Game3/Game3UI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game3UI : GameUI {
+
+	public UILabel messageLabel;
+	
+	public GameObject retryButton;
+
+
+//	public Game3Icon SetAnswer(int presentShape){
+//		GameObject puzzleObject = ((GameObject)GameObject.Instantiate(puzzlePrefab));
+//		puzzleObject.transform.SetParent(answer);
+//		transform.localScale = Vector3.one;
+//		Game2Icon puzzle = puzzleObject.GetComponent<Game2Icon>();
+//		puzzle.shapeNum = presentShape;
+//		puzzle.listId = -1;
+//		return puzzle;
+//	}
+//
+//	public Game3Icon MakePrefab(ref int presentlistId, int level, int answer){
+//		GameObject puzzleObject = ((GameObject)GameObject.Instantiate(puzzlePrefab));
+//		puzzleObject.transform.SetParent(puzzleParent);
+//		transform.localScale = Vector3.one;
+//		Game2Icon puzzle = puzzleObject.GetComponent<Game3Icon>();
+//		int shape = Random.Range(0, Mathf.Min(puzzleColors.Length, level+2));
+//		puzzle.shapeNum = shape == answer ? shape + 1 : shape;
+//		puzzle.listId = presentlistId;
+//		presentlistId ++ ;
+//		return puzzle;
+//	}
+}
