969f0ebe388f41f08e83519b5e856ed3d019c8d6
han sangeun <hsangeun4@trifort.jp>
2016-03-27 17:57:25 +0900

    XmlDeserializer追加

---
+++ alpha/Assets/Nobel/Scripts/NobelReader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NobelReader : MonoBehaviour {
+
+	[SerializeField]
+	Text nameArea;
+
+	[SerializeField]
+	Text textArea;
+
+	Phase phase;
+
+	public void Start() {
+		ReadPhase(1);
+	}
+
+	public void ReadPhase(int phaseNum) {
+		phase = XmlDeserializer.DeserializeXml<Phase>(((TextAsset)Resources.Load("Nobel" + phaseNum, typeof(TextAsset))).text);
+		Debug.Log(phase.charSerif[0].Name);
+	}
+}
---
+++ alpha/Assets/Nobel/Scripts/Phase.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("Nobel")]
+public class Phase {
+
+	public class Setting {
+		[XmlAttribute]
+		public string Background; 
+	}
+
+	public class CharSerif {
+		[XmlAttribute]
+		public string Name;
+
+		[XmlAttribute]
+		public string Image;
+	}
+
+
+	[XmlElement()]
+	public Setting setting;
+
+	[XmlElement()]
+	public List<CharSerif> charSerif;
+
+}
+
+public class XmlDeserializer
+{
+	public static T DeserializeXml<T> (string _xml_string) where T : class 
+	{
+		StringReader stringReader = new StringReader(_xml_string);			
+		XmlSerializer xmlSerializer = new XmlSerializer (typeof(T));
+		return (T)xmlSerializer.Deserialize(stringReader);
+	}
+}
+
